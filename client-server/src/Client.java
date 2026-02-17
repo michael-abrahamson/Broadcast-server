33fdb9372a5eddfe9d26340d720120e788c4ea4e
Michael Abrahamson <jonmichael.abrahamson@gmail.com>
2026-02-17 17:55:02 +0200

    Username creation added

--- client-server/src/Client.java
+++ client-server/src/Client.java
@@ -1,4 +1,5 @@
 import java.net.Socket;
+import java.util.Scanner;
 /**
  * This class is only ran by the client and is responsible for handling all client associated items such as messaging/broadcasting + receiving messages from the server.
  */
@@ -6,18 +7,41 @@ public class Client {
     private final String SERVER_ADDRESS = "localhost";
     private final int PORT = 5050;
 
-    
+    private String username;
+
+    private final Scanner scanner = new Scanner(System.in);
 
     public Client() {
+        this.username = "DefaultUser";
         System.out.println("Client is starting...");
 
         try (Socket socket = new Socket(SERVER_ADDRESS, PORT)) {
             System.out.println("Client is connected to server at: " + SERVER_ADDRESS + ":" + PORT);
+            
+            setUsername(username);
+            
             while (true) { 
                 
             }
         } catch (Exception e) {
             System.out.println("Error in client: " + e.getMessage());
         }
+    }
+
+    public void setUsername(String username) {
+
+        System.out.println("\nYour current username is: " + this.username);
+
+        System.out.print("Would you like to change your username? (y/n): ");
+
+        String input = scanner.nextLine();
+        if (input.equals("n")) {
+            return;
+        }
+        System.out.print("Enter your new username: ");
+        username = scanner.next();
+
+        this.username = username;
+        System.out.println("\nYour username has been updated to: " + this.username);
     }
 }
